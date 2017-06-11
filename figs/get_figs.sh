4d208f095c3be7a613b52c983adb02c59e40c4b7
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-06-11 13:09:34 +0100

    Add in a lot more figures for use.

--- figs/get_figs.sh
+++ figs/get_figs.sh
@@ -2,6 +2,15 @@
 HOST='mmuetz@login.rdf.ac.uk'
 DIR='um10.7_runs/postproc'
 SUITE='u-al000'
-URL="$HOST:$DIR/$SUITE/share/data/history"
+DATAM="$HOST:$DIR/$SUITE/share/data/history"
+DATAW="$HOST:$DIR/$SUITE/work/20000101T0000Z"
 
-rsync $URL/suite_results/figs/atmos.profile_plot.uv_profile.png .
+rsync -v $DATAM/suite_results/figs/atmos.profile_plot.uv_profile.png .
+rsync -v $DATAM/suite_results/figs/atmos.mass_flux_plot.z1_combined.png .
+rsync -v $DATAM/suite_results/figs/atmos.mass_flux_plot.z1_mf_weighted_comb.png .
+rsync -v $DATAM/suite_results/figs/atmos.mass_flux_spatial_scales_plot.z1_n1_combined.png .
+rsync -v $DATAM/suite_results/figs/atmos.mass_flux_spatial_scales_plot.S0.z1.all_n.hist.png .
+rsync -v $DATAM/suite_results/figs/atmos.profile_plot.thermodynamic_profile.png .
+rsync -v $DATAM/suite_results/figs/atmos.profile_plot.mf_profile.png .
+
+rsync -Rav "$DATAW/./S?_atmos/figs" .
