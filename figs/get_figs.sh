b2006a84a60115395ed0b2dc2355d00d17a964b9
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-06-11 19:28:18 +0100

    Add 2 new plots that are combination of previous plots.

--- figs/get_figs.sh
+++ figs/get_figs.sh
@@ -6,10 +6,12 @@ DATAM="$HOST:$DIR/$SUITE/share/data/history"
 DATAW="$HOST:$DIR/$SUITE/work/20000101T0000Z"
 
 rsync -v $DATAM/suite_results/figs/atmos.profile_plot.uv_profile.png .
-rsync -v $DATAM/suite_results/figs/atmos.mass_flux_plot.z1_combined.png .
-rsync -v $DATAM/suite_results/figs/atmos.mass_flux_plot.z1_mf_weighted_comb.png .
-rsync -v $DATAM/suite_results/figs/atmos.mass_flux_spatial_scales_plot.z1_n1_combined.png .
-rsync -v $DATAM/suite_results/figs/atmos.mass_flux_spatial_scales_plot.S0.z1.all_n.hist.png .
+# rsync -v $DATAM/suite_results/figs/atmos.mass_flux_plot.z1_combined.png .
+# rsync -v $DATAM/suite_results/figs/atmos.mass_flux_plot.z1_mf_weighted_comb.png .
+# rsync -v $DATAM/suite_results/figs/atmos.mass_flux_spatial_scales_plot.z1_n1_combined.png .
+# rsync -v $DATAM/suite_results/figs/atmos.mass_flux_spatial_scales_plot.S0.z1.all_n.hist.png .
+rsync -v $DATAM/suite_results/figs/atmos.mass_flux_plot.z1_both.png .
+rsync -v $DATAM/suite_results/figs/atmos.mass_flux_spatial_scales_plot.both_z1.png .
 rsync -v $DATAM/suite_results/figs/atmos.profile_plot.thermodynamic_profile.png .
 rsync -v $DATAM/suite_results/figs/atmos.profile_plot.mf_profile.png .
 
